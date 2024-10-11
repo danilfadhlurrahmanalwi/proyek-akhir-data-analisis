e77668bec8336de883ff6287c36ff79fdc88060f
Danil Fadhlurrahman Alwi <danilfadhlurrahmanalwi@gmail.com>
2024-10-11 22:27:01 +0700

    Update main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -41,8 +41,8 @@ def macem_season (day_df):
     season_df = day_df.groupby(by="season").count_cr.sum().reset_index() 
     return season_df
 
-day_df = pd.read_csv("day_df.csv")
-hour_df = pd.read_csv("hour_df.csv")
+day_df = pd.read_csv('day_df.csv')
+hour_df = pd.read_csv('hour_df.csv')
 
 datetime_columns = ["dteday"]
 day_df.sort_values(by="dteday", inplace=True)
